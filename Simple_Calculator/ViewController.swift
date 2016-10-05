dc2c473e7269c6420fa46898c369c77e565f1711
jaems33 <james.teow83@gmail.com>
2016-10-05 15:59:07 -0400

    Testing committing to github by altering file.

--- Simple_Calculator/ViewController.swift
+++ Simple_Calculator/ViewController.swift
@@ -28,6 +28,8 @@ class ViewController: UIViewController {
     
     }
   
+    
+    // Adding sample comment
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
