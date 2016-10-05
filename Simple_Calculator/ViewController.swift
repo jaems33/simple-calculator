fa26edc0501dc4d1a48f728ee37d89046327beca
jaems33 <james.teow83@gmail.com>
2016-10-05 15:56:19 -0400

    Initial Commit

---
+++ Simple_Calculator/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Simple_Calculator
+//
+//  Created by James Teow on 10/4/16.
+//  Copyright © 2016 James Teow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var gradientLayer: CAGradientLayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        createGradientLayer()
+        
+        digit.setTitleColor(UIColor.white, for: .normal)
+        digit.layer.borderColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+        
+    }
+    
+    @IBOutlet weak var digit: UIButton!
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+    
+    }
+  
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func createGradientLayer() {
+        gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.view.bounds
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        let top = UIColor(red: 60.0/255.0, green: 75.0/255.0, blue: 148.0/255.0, alpha: 1.0).cgColor
+        let bottom = UIColor(red: 93.0/255.0, green: 192.0/255.0, blue: 235.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.colors = [top, bottom]
+        self.view.layer.addSublayer(gradientLayer)
+        gradientLayer.zPosition = -1
+    }
+
+    @IBAction func PanView(sender: UIScreenEdgePanGestureRecognizer) {
+        let top = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+        let bottom = UIColor(red: 93.0/255.0, green: 192.0/255.0, blue: 235.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.colors = [top, bottom]
+    }
+}
+
